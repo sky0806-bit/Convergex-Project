31e3bec8739a0577abc7bd8c4eb8c55539ed31ad
sky0806-bit <ysiddharth83@gmail.com>
2024-12-01 14:02:21 +0530

    Add files via upload
    This is not our final product,just a prototype or say a intro that what we are going to do.... Final product of our project is going to be a super lit .
    #Unstoppable

---
+++ PersonalInformation.js
@@ -0,0 +1,4 @@
+// This script can be extended for dynamic functionalities if needed.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Personal Information Form Loaded");
+});
